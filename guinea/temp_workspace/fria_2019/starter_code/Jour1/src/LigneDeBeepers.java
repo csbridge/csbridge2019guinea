05271a8afd23fafe93cce3bc66ac0e02041900f7
Moussa Doumbouya <doumbouya.moussa@gmail.com>
2019-12-15 16:23:18 +0000

    Projet Mountain Karel et exemples

--- guinea/temp_workspace/fria_2019/starter_code/Jour1/src/LigneDeBeepers.java
+++ guinea/temp_workspace/fria_2019/starter_code/Jour1/src/LigneDeBeepers.java
@@ -1,4 +1,10 @@
-import stanford.karel.*;
+/*
+ * Programme: LigneDeBeepers
+ * ----------
+ * Placer des beepers sur toute la premiere ligne du monde de Karel.
+ * Suppose que Karel commence dans le coin inférieur gauche et fait face a l'Est.
+ */
+import stanford.karel.SuperKarel;
 
 public class LigneDeBeepers extends SuperKarel {
 	
@@ -7,6 +13,12 @@ public class LigneDeBeepers extends SuperKarel {
 			putBeeper();
 			move();
 		}
+		
+		// La ligne de code suivante est nécessaire pour placer le beeper final.
+		// A chaque iteration, Karel place un beeper dans la cellule courante 
+		// et se déplace pour la cellule suivante.
+		// Cette ligne de code place un beeper dans la derniere
+		// cellule dans laquelle Karel s'est positioné.
 		putBeeper();
 	}
 
