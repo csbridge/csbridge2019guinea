5cbc3c0c996e24d736b0dde2985f2b8185871f75
Moussa Doumbouya <doumbouya.moussa@gmail.com>
2019-12-15 11:52:05 +0000

    Exemple: Monter l'escalier en francais

--- guinea/temp_workspace/fria_2019/starter_code/Jour1/src/MonterEscalier.java
+++ guinea/temp_workspace/fria_2019/starter_code/Jour1/src/MonterEscalier.java
@@ -1,9 +1,38 @@
 import stanford.karel.*;
 
+/* Programme: Monter un escalier
+ * ----------
+ * Votre premier exemple de programme Karel. Ce programme 
+ * demande a Karel de prendre le beeper devant lui, et de
+ * le placer sur la marche de l'escallier.
+ * Ceci est un commentaire. Il sera ignoré pendant l'execution
+ * du programme
+ */
 public class MonterEscalier extends Karel {
 	
+	/*
+	 * Lorsque vous démarrez votre programme, cette 
+	 * méthode sera executée.
+	 */
 	public void run() {
-		// your code here...
+		move();
+		pickBeeper();
+		turnLeft();
+		move();
+		turnRight();
+		move();
+		putBeeper();
+		move();
+	}
+	
+	/*
+	 * Definition d'une nouvelle commande (méthode)
+	 * qui permet à Karel de tourner a droite
+	 */
+	private void turnRight() {
+		turnLeft();
+		turnLeft();
+		turnLeft();
 	}
 
 }
